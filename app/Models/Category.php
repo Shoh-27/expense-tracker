f640cd826a9d46397ebeb9ef18e4b6ea7c91bb6d
Shohrux <shohruxnizomov170@gmail.com>
2025-09-15 15:51:38 +0500

    category va transaction modellari yozildi

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name' , 'type'];
+
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = ['user_id' , 'category_id' , 'kind' , 'amount' , 'note' , 'date'];
+
+    protected $casts = [
+        'date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
